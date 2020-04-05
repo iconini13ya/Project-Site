651f6585702f88a6005e54d7d63f33e2b8f0740d
Yarancev Vladimir <gglobalink@gmail.com>
2020-04-06 01:40:07 +0300

    Мой сайт на node js
    ДОбавил bootstrap

---
+++ server.js
@@ -0,0 +1,10 @@
+var express= require("express");
+
+var app= express();
+app.use(express.static('Main_folder'));
+
+app.get('/',function(req,res){
+    res.sendfile('Main_folder/index.html');
+});
+//SSS
+app.listen(3000,function(){console.log("Start Server")});
